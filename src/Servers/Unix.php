d0eadb94ccde3fb0b1d4529da73422bb0a45fad7
Baptiste Langlade <baptouuuu@gmail.com>
2017-05-29 08:47:14 +0200

    add unix server

---
+++ src/Servers/Unix.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\Server\Control\Servers;
+
+use Innmind\Server\Control\{
+    Server,
+    Server\Processes,
+    Server\Processes\UnixProcesses
+};
+
+final class Unix implements Server
+{
+    private $processes;
+
+    public function __construct()
+    {
+        $this->processes = new UnixProcesses;
+    }
+
+    public function processes(): Processes
+    {
+        return $this->processes;
+    }
+}
