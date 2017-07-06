b920d275a1fc0f0f9e3ff869bc0155a2eef4d7ee
Baptiste Langlade <baptouuuu@gmail.com>
2017-07-06 17:56:41 +0200

    add background process

---
+++ tests/Server/Process/BackgroundProcessTest.php
@@ -0,0 +1,88 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\Server\Control\Server\Process;
+
+use Innmind\Server\Control\{
+    Server\Process\BackgroundProcess,
+    Server\Process as ProcessInterface,
+    Server\Process\Pid,
+    Server\Process\ExitCode,
+    Server\Process\Output\StaticOutput,
+    Server\Process\Output\Type,
+    Exception\ProcessStillRunning
+};
+use Innmind\Immutable\Str;
+use Symfony\Component\Process\Process as SfProcess;
+use PHPUnit\Framework\TestCase;
+
+class BackgroundProcessTest extends TestCase
+{
+    public function testInterface()
+    {
+        $process = new SfProcess('ps &');
+        $process->start();
+
+        $this->assertInstanceOf(
+            ProcessInterface::class,
+            new BackgroundProcess($process)
+        );
+    }
+
+    /**
+     * @expectedException Innmind\Server\Control\Exception\BackgroundProcessInformationNotAvailable
+     */
+    public function testPid()
+    {
+        $ps = new SfProcess('ps &');
+        $ps->start();
+        $process = new BackgroundProcess($ps);
+
+        $process->pid();
+    }
+
+    public function testOutput()
+    {
+        $slow = new SfProcess('php fixtures/slow.php &');
+        $slow->start();
+        $process = new BackgroundProcess($slow);
+
+        $this->assertInstanceOf(StaticOutput::class, $process->output());
+        $start = time();
+        $this->assertSame('', (string) $process->output());
+        $this->assertTrue((time() - $start) < 1);
+    }
+
+     /**
+     * @expectedException Innmind\Server\Control\Exception\BackgroundProcessInformationNotAvailable
+     */
+    public function testExitCode()
+    {
+        $slow = new SfProcess('php fixtures/slow.php &');
+        $slow->start();
+        $process = new BackgroundProcess($slow);
+
+        $process->exitCode();
+    }
+
+    public function testWait()
+    {
+        $slow = new SfProcess('php fixtures/slow.php');
+        $slow->start();
+        $process = new BackgroundProcess($slow);
+
+        $this->assertSame($process, $process->wait());
+    }
+
+     /**
+     * @expectedException Innmind\Server\Control\Exception\BackgroundProcessInformationNotAvailable
+     */
+    public function testIsRunning()
+    {
+        $slow = new SfProcess('php fixtures/slow.php');
+        $slow->start();
+        $process = new BackgroundProcess($slow);
+
+        $process->isRunning();
+    }
+}
